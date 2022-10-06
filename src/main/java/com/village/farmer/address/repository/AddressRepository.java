09d40c769d49e91082c3072784349faf964a6bd9
Lustaria <hunter0darkness@gmail.com>
2022-10-06 15:28:07 +0700

    remove all

--- src/main/java/com/village/farmer/address/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.village.farmer.address.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.village.farmer.address.entity.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-
-}
--- src/main/java/com/village/farmer/career/repository/CareerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.village.farmer.career.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.village.farmer.career.entity.Careers;
-
-public interface CareerRepository extends JpaRepository<Careers, Integer> {
-
-}
