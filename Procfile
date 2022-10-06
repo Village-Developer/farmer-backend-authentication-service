10a8f1e5d249ea490bf0d81e92d31d718da52ce6
Lustaria <hunter0darkness@gmail.com>
2022-10-06 16:18:04 +0700

    ลบทำตัวเปล่า

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/farmer-0.0.1-SNAPSHOT.jar
--- src/main/java/com/village/farmer/products/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.village.farmer.products.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.village.farmer.products.entity.Product;
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
